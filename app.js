400226e981dea51e81e95c741ed70cce904f5512
GMaxDev <maxime.guigra@gmail.com>
2022-10-22 18:14:36 +0200

    création des modèles de sauce et d'utilisateur

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express')
-const { nextTick } = require('process')
+const mongoose = require('mongoose')
 
+//on se connecte à la base de donnée
+mongoose.connect('mongodb+srv://GMaxDev:VtUflowp6X4kSJ6N@cluster0.dn1raoi.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+//on initialise express
 const app = express()
+
+//middleware permettant d'accéder à l'API depuis n'importe où, d'ajouter des headers et d'envoyer des requêtes avec certaines méthode
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next()
+});
+
+//------------------------------
 
 app.use((req, res, next) =>{
     console.log('requete recu')
