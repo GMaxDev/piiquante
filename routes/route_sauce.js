270e28fdccf412c26bd4fc85bd5162addc94afc2
GMaxDev <maxime.guigra@gmail.com>
2022-10-23 10:33:31 +0200

    ajout des middlewares

--- routes/route_sauce.js
+++ routes/route_sauce.js
@@ -1,3 +1,6 @@
 const express = require('express')
-const routerSauce = express.Router()
 const Sauces = require('../models/model_sauce');
+const routerSauce = express.Router()
+
+const auth = require('../middlewares/auth')
+const multer = require ('../middlewares/multer-config')
